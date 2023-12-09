2f5c9b89ffbbf8086d06f5b0932b2ac6aa70f731
ir2718 <irep2718@gmail.com>
2023-12-09 16:41:15 +0100

    day 9

---
+++ src/day_9/problem2.py
@@ -0,0 +1,19 @@
+with open("files/day-9.txt", "r") as f:
+    lines = [list(map(int, x.strip().split())) for x in f]
+
+
+final_res = 0
+for l in lines:
+    curr_diffs = l
+    all_diffs = [curr_diffs]
+    while len(set(curr_diffs)) != 1:
+        curr_diffs = [curr_diffs[i] - curr_diffs[i-1] for i in range(1, len(curr_diffs))]
+        all_diffs.append(curr_diffs)
+
+
+    res = 0
+    for i in range(len(all_diffs)-1, -1, -1):
+        res = all_diffs[i][0] - res
+
+    final_res += res
+print(final_res)
